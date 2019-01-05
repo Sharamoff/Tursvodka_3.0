d3510c8ef25069e165a9cdcee4ecd023b0d7fd7f
Ihar Sharamau <sharamoff@gmail.com>
2019-01-05 11:28:11 +0300

    gulp4 syntaxis changed

--- _PROJECT/gulpfile.js
+++ _PROJECT/gulpfile.js
@@ -1,20 +1,19 @@
 'use strict';
 
 var gulp = require('gulp'),
-		sass = require('gulp-sass'),
-		watch = require('gulp-watch'),
-		plumber = require('gulp-plumber'),
-		prefixer = require('gulp-autoprefixer'),
-		sourcemaps = require('gulp-sourcemaps'),
-		rigger = require('gulp-rigger'),
-		uglify = require('gulp-uglify'),
-		cssmin = require('gulp-clean-css'),
-		imagemin = require('gulp-imagemin'),
-		cache = require('gulp-cache'),
-		pngquant = require('imagemin-pngquant'),
-		rimraf = require('rimraf'),
-		browserSync = require("browser-sync"),
-		reload = browserSync.reload;
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    prefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
 
 var path = {
 	build: {
@@ -51,23 +50,15 @@ var config = {
 	logPrefix: "Promoting"
 };
 
-gulp.task('webserver', function () {
-	browserSync(config);
-});
-
-gulp.task('clean', function (cb) {
-	rimraf(path.clean, cb);
-});
-
 gulp.task('html:build', function () {
-	gulp.src(path.src.html)
+	return gulp.src(path.src.html)
 			.pipe(rigger())
 			.pipe(gulp.dest(path.build.html))
 			.pipe(reload({stream: true}));
 });
 
 gulp.task('js:build', function () {
-	gulp.src(path.src.js)
+	return gulp.src(path.src.js)
 			.pipe(rigger())
 			//.pipe(sourcemaps.init())
 			//.pipe(uglify()) !не минимизируем
@@ -77,7 +68,7 @@ gulp.task('js:build', function () {
 });
 
 gulp.task('style:build', function () {
-	gulp.src(path.src.style)
+	return gulp.src(path.src.style)
 			.pipe(plumber())
 			//.pipe(sourcemaps.init())
 			.pipe(sass({
@@ -92,7 +83,7 @@ gulp.task('style:build', function () {
 });
 
 gulp.task('image:build', function () {
-	gulp.src(path.src.img)
+	return gulp.src(path.src.img)
 			.pipe(cache(imagemin({
 				progressive: true,
 				svgoPlugins: [{removeViewBox: false}],
@@ -104,36 +95,37 @@ gulp.task('image:build', function () {
 });
 
 gulp.task('fonts:build', function() {
-	gulp.src(path.src.fonts)
+	return gulp.src(path.src.fonts)
 			.pipe(gulp.dest(path.build.fonts))
 });
 
-gulp.task('build', [
-	'html:build',
-	'js:build',
-	'style:build',
-	'fonts:build',
-	'image:build'
-]);
+gulp.task('build', gulp.parallel(
+		'html:build',
+		'js:build',
+		'style:build',
+		'fonts:build',
+		'image:build'
+	)
+);
 
 
-gulp.task('watch', function(){
-	watch([path.watch.html], function(event, cb) {
-		gulp.start('html:build');
-	});
-	watch([path.watch.style], function(event, cb) {
-		gulp.start('style:build');
-	});
-	watch([path.watch.js], function(event, cb) {
-		gulp.start('js:build');
-	});
-	watch([path.watch.img], function(event, cb) {
-		gulp.start('image:build');
-	});
-	watch([path.watch.fonts], function(event, cb) {
-		gulp.start('fonts:build');
-	});
+gulp.task('watch', function(done) {
+	gulp.watch(path.watch.html,   gulp.series('html:build'));
+	gulp.watch(path.watch.style,  gulp.series('style:build'));
+	gulp.watch(path.watch.js,     gulp.series('js:build'));
+	gulp.watch(path.watch.image,  gulp.series('image:build'));
+	gulp.watch(path.watch.fonts,  gulp.series('fonts:build'));
+	done();
 });
 
 
-gulp.task('default', ['build', 'webserver', 'watch']);
+gulp.task('webserver', function () {
+	browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+	rimraf(path.clean, cb);
+});
+
+
+gulp.task('default', gulp.series('build', 'webserver', 'watch'));
